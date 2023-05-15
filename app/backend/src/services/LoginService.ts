bd03af37ec99ebc20981725a44a089157590e56d
Bernardo Martorano <bernardormartorano@gmail.com>
2023-05-14 22:32:58 -0300

    login requirements

--- app/backend/src/services/LoginService.ts
+++ app/backend/src/services/LoginService.ts
@@ -2,14 +2,15 @@ import { compare } from 'bcryptjs';
 import UserModel from '../database/models/User';
 import User from '../entities/UserEntities/User';
 import InvalidFieldsError from '../errorClasses.ts/InvalidFields';
+import newToken from '../utils/auth';
 
 export default class LoginService {
   static async login(email: string, password: string) {
     const user = new User(email, password);
-    const userChecked = await UserModel.findOne({ where: { email: user.email } });
-    if (!userChecked) throw new InvalidFieldsError('Invalid email or password');
-    const passwordMatch = await compare(user.password, userChecked.password);
-    console.log('******* As senhas são iguais? ', passwordMatch);
-    return 'token';
+    const userMatched = await UserModel.findOne({ where: { email: user.email } });
+    if (!userMatched) throw new InvalidFieldsError('Invalid email or password');
+    const passwordMatch = await compare(user.password, userMatched.password);
+    if (!passwordMatch) throw new InvalidFieldsError('Invalid email or password');
+    return newToken({ id: userMatched.id, email: userMatched.email });
   }
 }
