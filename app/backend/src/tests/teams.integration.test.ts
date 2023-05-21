7635c3075050d42b518a2f85fe768aa117d1b517
Bernardo Martorano <bernardormartorano@gmail.com>
2023-05-21 18:54:49 -0300

    getAllTeams test ready

--- app/backend/src/tests/teams.integration.test.ts
+++ app/backend/src/tests/teams.integration.test.ts
@@ -14,12 +14,18 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Testes de integração da rota Teams', () => {
+describe('Testes de integração da rota Teams', async () => {
   afterEach(() => {
     sinon.restore();
   });
 
-  it('A rota /teams deve retornar todos os times cadastrados', () => {
-    sinon.stub(TeamModel, 'findAll').resolves([{dataValues:}])
+  it('A rota /teams deve retornar todos os times cadastrados', async () => {
+    sinon.stub(TeamModel, 'findAll').resolves(teamsMock as TeamModel[])
+
+    const response = await chai.request(app).get('/teams');
+
+    expect(response.status).to.be.equal(200);
+    expect(response.body).to.be.deep.equal(teamsMock);
   });
+
 });
